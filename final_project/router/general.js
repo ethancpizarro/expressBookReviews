d854bc8060dc9f72faa3f3a8059fd3244545edd8
Ethan Pizarro <ethancpizarro2@outlook.com>
2025-07-16 15:59:57 -0400

    Axios added explicitly

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
@@ -30,13 +31,10 @@ public_users.post("/register", (req,res) => {
 public_users.get('/', async function (req, res) {
     try
     {
-        const getBooks = () => {
-            return new Promise((resolve, reject) => {
-                resolve(books);
-            });
-        };
-    
-        const bookList = await getBooks();
+        // make an HTTP GET request to my own server's book list endpoint
+        const response = await axios.get('http://localhost:5000/');
+        const bookList = response.data;
+
         return res.status(200).send(JSON.stringify(bookList, null, 4));
     }
     catch (err)
@@ -49,29 +47,24 @@ public_users.get('/', async function (req, res) {
 public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
 
-    // Simulate asynchronous data fetching
-    const getBookByISBN = (isbn) => {
-        return new Promise((resolve, reject) => {
-            const book = books[isbn];
-            if (book)
-            {
-                resolve(book);
-            }
-            else
-            {
-                reject("Book not found");
-            }
-        });
-    };
-
     try
     {
-        const bookData = await getBookByISBN(isbn);
+        // make an HTTP GET request to my own server's ISBN endpoint
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+        const bookData = response.data;
         return res.status(200).json(bookData);
     }
     catch (err)
     {
-        return res.status(404).json({ message: err });
+        // handle errors such as 404 or server errors
+        if (err.response && err.response.status === 404)
+        {
+            return res.status(404).json({ message: "Book not found" });
+        }
+        else
+        {
+            return res.status(500).json({ message: "Error retrieving book details" });
+        }
     }
  });
   
@@ -79,36 +72,23 @@ public_users.get('/isbn/:isbn', async function (req, res) {
 public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
 
-    const getBooksByAuthor = (author) => {
-        return new Promise((resolve, reject) => {
-            const booksByAuthor = [];
-            for (const key in books)
-            {
-                if (books[key].author.toLowerCase() === author.toLowerCase())
-                {
-                    booksByAuthor.push({ isbn: key, ...books[key] });
-                }
-            }
-
-            if (booksByAuthor.length > 0)
-            {
-                resolve(booksByAuthor);
-            }
-            else
-            {
-                reject("No books found for this author");
-            }
-        });
-    };
-
     try
     {
-        const result = await getBooksByAuthor(author);
-        return res.status(200).json(result);
+        // make an HTTP GET request to my own server's author endpoint
+        const response = await axios.get(`http://localhost:5000/author/${encodeURIComponent(author)}`);
+        const booksByAuthor = response.data;
+        return res.status(200).json(booksByAuthor);
     }
     catch (err)
     {
-        return res.status(404).json({ message: err });
+        if (err.response && err.response.status === 404)
+        {
+            return res.status(404).json({ message: "No books found for this author" });
+        }
+        else
+        {
+            return res.status(500).json({ message: "Error retrieving books by author" });
+        }
     }
 });
 
@@ -116,36 +96,23 @@ public_users.get('/author/:author', async function (req, res) {
 public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
 
-    const getBooksByTitle = (title) => {
-        return new Promise((resolve, reject) => {
-            const booksByTitle = [];
-            for (const key in books)
-            {
-                if (books[key].title.toLowerCase() === title.toLowerCase())
-                {
-                    booksByTitle.push({ isbn: key, ...books[key] });
-                }
-            }
-
-            if (booksByTitle.length > 0)
-            {
-                resolve(booksByTitle);
-            }
-            else
-            {
-                reject("No books found with this title");
-            }
-        });
-    };
-
     try
     {
-        const result = await getBooksByTitle(title);
-        return res.status(200).json(result);
+        // make an HTTP GET request to my own server's title endpoint
+        const response = await axios.get(`http://localhost:5000/title/${encodeURIComponent(title)}`);
+        const booksByTitle = response.data;
+        return res.status(200).json(booksByTitle);
     }
     catch (err)
     {
-        return res.status(404).json({ message: err });
+        if (err.response && err.response.status === 404)
+        {
+            return res.status(404).json({ message: "No books found with this title" });
+        }
+        else
+        {
+            return res.status(500).json({ message: "Error retrieving books by title" });
+        }
     }
 });
 
